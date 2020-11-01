7df7ab1a9caa7426936cd01493908a97ce0902b2
NitinAgarwal691 <73051793+NitinAgarwal691@users.noreply.github.com>
2020-11-01 18:15:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var rect1,rect2,rect3;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	World.add(world, packageBody);
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	boxPosition=width/2-100;
+	boxY=610;
+	boxLeftSprite=createSprite(boxPosition,boxY,20,100);
+	boxLeftSprite.shapeColor="green";
+	
+	boxBottomSprite=createSprite(boxPosition+100,boxY+40,200,20);
+    boxBottomSprite.shapeColor="green";
+
+	boxRightSprite=createSprite(boxPosition+200,boxY,20,100);
+	boxRightSprite.shapeColor="green";
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y;
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+if(keyCode===LEFT_ARROW) {
+	helicopterSprite.x=helicopterSprite.x-21;
+	translation={x:-20,y:0}
+	Matter.Body.translate(packageBody,translation)
+}else if(keyCode===RIGHT_ARROW){
+	helicopterSprite.x=helicopterSprite.x+21;
+	translation={x:+20,y:0}
+	Matter.Body.translate(packageBody,translation)
+}else if (keyCode === DOWN_ARROW) {
+	// Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody,false);
+  }
+}
